7f7815bd075cc5b0b56c891f98d5e4ffa88de5ce
Xinran Tang <shyxt1@nottingham.edu.cn>
2021-04-24 11:18:08 +0800

    feat: create project

---
+++ cmake-build-debug/CMakeFiles/PSO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PSO.dir/main.c.obj"
+  "PSO.exe"
+  "PSO.exe.manifest"
+  "PSO.pdb"
+  "libPSO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PSO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
